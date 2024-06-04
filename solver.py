eb978c02e0b16906ee6df0fdad2c1e301a5522c3
Erfan <golpourerfan@gmail.com>
2024-06-03 21:11:24 -0700

    Rename 'tiles' to 'clusters'

--- solver.py
+++ solver.py
@@ -13,10 +13,10 @@ def get_combinations(lst: Set) -> Iterable[Tuple]:
     return combinations
 
 
-class Tile:
+class Cluster:
     """
-    A collection of cells in a superposition.
-    The existence of a mine is known within the tile, but not its location.
+    An undeterministic collection of cells.
+    The existence of a mine is known within the cluster, but not its location.
     """
 
     def __init__(self, cells: Set[Cell], n_mine: int):
@@ -32,108 +32,110 @@ class Tile:
 class Solver:
     def __init__(self, board: Board):
         self.board = board
-        self.tiles: Sequence[Tile] = []
-        self.tile_map: Dict[Cell, Set[Tile]] = defaultdict(set)
+        self.clusters: Sequence[Cluster] = []
+        self.cluster_map: Dict[Cell, Set[Cluster]] = defaultdict(set)
 
     def solve(self) -> bool:
         while self.propagate_known_values():
             continue
         return self.board.check_win()
 
-    def get_neighbour_tiles(self, cells: Set[Cell]) -> Set[Tile]:
-        """return any tile that has a cell in common with the given cells"""
-        return {tile for neighbour in cells for tile in self.tile_map[neighbour]}
+    def get_neighbour_clusters(self, cells: Set[Cell]) -> Set[Cluster]:
+        """return any cluster that has a cell in common with the given cells"""
+        return {
+            cluster for neighbour in cells for cluster in self.cluster_map[neighbour]
+        }
 
     def propagate_known_values(self) -> bool:
         to_reveal: Set[Cell] = set()
         to_flag: Set[Cell] = set()
 
-        # Generate tiles naively
-        self.tiles = []
-        self.tile_map.clear()
+        # Generate clusters naively
+        self.clusters = []
+        self.cluster_map.clear()
         for cell in self.board.revealed:
             neighbours = self.board.get_neighbour_cells(cell)
             if unknown_neighbours := {n for n in neighbours if not n.known}:
-                tile = Tile(unknown_neighbours, cell.mines - cell.flags)
+                cluster = Cluster(unknown_neighbours, cell.mines - cell.flags)
                 for neighbour in unknown_neighbours:
-                    self.tile_map[neighbour].add(tile)
-                self.tiles.append(tile)
+                    self.cluster_map[neighbour].add(cluster)
+                self.clusters.append(cluster)
 
-        # Refactor tiles:
-        # This will effectively subtract subtiles from larger tiles, dividing
+        # Refactor clusters:
+        # This will effectively subtract subclusters from larger clusters, dividing
         # them into their building blocks. This can greatly assist the logic engine
         changes = True
         while changes:
             changes = False
-            new_tiles: Sequence[Tile] = []
-            refactored_tiles: Dict[Cell, Set[Tile]] = defaultdict(set)
-            for tile in self.tiles:
-                if tile.mine_perc == 0:
+            new_clusters: Sequence[Cluster] = []
+            refactored_clusters: Dict[Cell, Set[Cluster]] = defaultdict(set)
+            for cluster in self.clusters:
+                if cluster.mine_perc == 0:
                     # trivial: all cells are safe
-                    to_reveal |= tile.cells
-                elif tile.mine_perc == 1:
+                    to_reveal |= cluster.cells
+                elif cluster.mine_perc == 1:
                     # trivial: every cell is a mine
-                    to_flag |= tile.cells
+                    to_flag |= cluster.cells
                 else:
-                    # remove any neighbour tile contained within this tile from it
-                    for neighbour_tile in self.get_neighbour_tiles(tile.cells):
-                        if neighbour_tile.cells < tile.cells:
-                            tile.mines -= neighbour_tile.mines
-                            tile.cells -= neighbour_tile.cells
+                    # remove any neighbour cluster contained within this cluster from it
+                    for neighbour_cluster in self.get_neighbour_clusters(cluster.cells):
+                        if neighbour_cluster.cells < cluster.cells:
+                            cluster.mines -= neighbour_cluster.mines
+                            cluster.cells -= neighbour_cluster.cells
                             changes = True
 
-                    if tile.cells:
-                        for cell in tile.cells:
-                            refactored_tiles[cell].add(tile)
-                        new_tiles.append(tile)
+                    if cluster.cells:
+                        for cell in cluster.cells:
+                            refactored_clusters[cell].add(cluster)
+                        new_clusters.append(cluster)
 
-            self.tiles = new_tiles
-            self.tile_map = refactored_tiles
+            self.clusters = new_clusters
+            self.cluster_map = refactored_clusters
 
         for cell in self.board.revealed:
             if unknown_neighbours := {
                 n for n in self.board.get_neighbour_cells(cell) if not n.known
             }:
-                tile_combinations = get_combinations(
-                    self.get_neighbour_tiles(unknown_neighbours)
+                cluster_combinations = get_combinations(
+                    self.get_neighbour_clusters(unknown_neighbours)
                 )
 
                 def get_common_cells(
-                    tiles: Sequence[Tile],
+                    clusters: Sequence[Cluster],
                 ) -> Set[Cell]:
                     common_cells: Set[Cell] = set()
-                    for i in range(len(tiles) - 1):
-                        for j in range(i + 1, len(tiles)):
-                            common_cells |= tiles[i].cells & tiles[j].cells
+                    for i in range(len(clusters) - 1):
+                        for j in range(i + 1, len(clusters)):
+                            common_cells |= clusters[i].cells & clusters[j].cells
                     return common_cells
 
-                # avoid overlapping tile combinations
-                for tile_comb in {
-                    tile_comb
-                    for tile_comb in tile_combinations
-                    if not get_common_cells(tile_comb)
+                # avoid overlapping cluster combinations
+                for cluster_comb in {
+                    cluster_comb
+                    for cluster_comb in cluster_combinations
+                    if not get_common_cells(cluster_comb)
                 }:
-                    tile_comb_mines = sum(tile.mines for tile in tile_comb)
-                    tile_comb_cells = {
-                        cell for tile in tile_comb for cell in tile.cells
+                    cluster_comb_mines = sum(cluster.mines for cluster in cluster_comb)
+                    cluster_comb_cells = {
+                        cell for cluster in cluster_comb for cell in cluster.cells
                     }
-                    # if the tile permits less mines than the cell needs
+                    # if the cluster permits less mines than the cell needs
                     # and the remaining neighbours are just enough to satisfy
                     # the difference, the remaining neighbours are mines
                     if (
-                        tile_comb_mines < cell.mines - cell.flags
-                        and len(unknown_neighbours - tile_comb_cells)
-                        == cell.mines - cell.flags - tile_comb_mines
+                        cluster_comb_mines < cell.mines - cell.flags
+                        and len(unknown_neighbours - cluster_comb_cells)
+                        == cell.mines - cell.flags - cluster_comb_mines
                     ):
-                        to_flag |= unknown_neighbours - tile_comb_cells
-                    # if the mines introduced by the tile are contained by
+                        to_flag |= unknown_neighbours - cluster_comb_cells
+                    # if the mines introduced by the cluster are contained by
                     # the cell's neighbourhood AND satisfy the cell, the
                     # remaining neighbours are safe
                     elif (
-                        tile_comb_cells < unknown_neighbours
-                        and tile_comb_mines == cell.mines - cell.flags
+                        cluster_comb_cells < unknown_neighbours
+                        and cluster_comb_mines == cell.mines - cell.flags
                     ):
-                        to_reveal |= unknown_neighbours - tile_comb_cells
+                        to_reveal |= unknown_neighbours - cluster_comb_cells
 
         if len(to_reveal) == 0 and len(to_flag) == 0:
             # final checks based on the number of unflagged mines
@@ -145,10 +147,10 @@ class Solver:
                     # all remaining cells must be mines
                     to_flag |= unknown_cells
                 else:
-                    # if one tile alone can satisfy the board, any other cell is safe
-                    for tile in self.get_neighbour_tiles(unknown_cells):
-                        if tile.mines == self.board.unflagged:
-                            to_reveal |= unknown_cells - tile.cells
+                    # if one cluster alone can satisfy the board, any other cell is safe
+                    for cluster in self.get_neighbour_clusters(unknown_cells):
+                        if cluster.mines == self.board.unflagged:
+                            to_reveal |= unknown_cells - cluster.cells
                     if not to_reveal:
                         return False  # no advancement could be made
             else:
@@ -163,17 +165,17 @@ class Solver:
         return True
 
     def make_guess(self) -> Cell:
-        # if forced, choose a random cell from a tile with least mine percentage
-        best_tile = self.tiles[0]
-        untiled_mines = self.board.unflagged
-        untiled_cells = self.board.unrevealed - self.board.flagged
-        for tile in self.tiles:
-            untiled_cells -= tile.cells
-            untiled_mines -= tile.mines
-            if tile.mine_perc < best_tile.mine_perc:
-                best_tile = tile
+        # if forced, choose a random cell from a cluster with least mine percentage
+        best_cluster = self.clusters[0]
+        unclusterd_mines = self.board.unflagged
+        unclusterd_cells = self.board.unrevealed - self.board.flagged
+        for cluster in self.clusters:
+            unclusterd_cells -= cluster.cells
+            unclusterd_mines -= cluster.mines
+            if cluster.mine_perc < best_cluster.mine_perc:
+                best_cluster = cluster
 
-        if best_tile.mine_perc > untiled_mines / len(untiled_cells):
-            # it is safer to choose a cell from untiled territory
-            return rand_choice(list(untiled_cells))
-        return rand_choice(list(best_tile.cells))
+        if best_cluster.mine_perc > unclusterd_mines / len(unclusterd_cells):
+            # it is safer to choose a cell from unclusterd territory
+            return rand_choice(list(unclusterd_cells))
+        return rand_choice(list(best_cluster.cells))
